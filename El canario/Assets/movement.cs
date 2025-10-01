85a93e32813f7a0adb945f1ba1ede6c006d98089
Paulalemany <palemany@ucm.es>
2025-10-01 18:59:45 +0200

    Se mueve :)
    Co-Authored-By: (i)nés <125393230+ineprimo@users.noreply.github.com>

---
+++ El canario/Assets/movement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movement : MonoBehaviour
+{
+    CharacterController characterController;
+    Vector2 moveInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movePos = new Vector3(moveInput.x, 0, moveInput.y);
+        characterController.Move(movePos * Time.deltaTime * 2);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
