99afdd8f7e6e27aac58d04ae67cf0c207c883415
Paulalemany <palemany@ucm.es>
2025-10-08 20:06:23 +0200

    Lo dejamos por hoy

--- El canario/Assets/Scripts/Hoja4/VolumeControler.cs
+++ El canario/Assets/Scripts/Hoja4/VolumeControler.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 
 public class VolumeControler : MonoBehaviour
@@ -9,40 +10,52 @@ public class VolumeControler : MonoBehaviour
     [SerializeField]
     float vol = 0.05f;
 
-    AudioSource source;
-
     [SerializeField]
     float time = 2f;
+
+    [SerializeField]
+    AudioSource [] source;
+
+    AudioSource act;
+
+    bool canFade = true;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        source = GetComponent<AudioSource>();
+        for (int i = 0; i < source.Length; i++)
+            source[i].volume = 0;
+        act = source[0];
+        act.volume = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+       
     }
 
     public void changeUp(InputAction.CallbackContext context)
     {
-        source.volume += vol;
+        act.volume += vol;
     }
 
     public void changeDown(InputAction.CallbackContext context)
     {
-        source.volume -= vol;
+        act.volume -= vol;
     }
 
     public void fadeOut(InputAction.CallbackContext context)
     {
-        StartCoroutine(StartFade(source, time, 0));
+        if (canFade)
+        {
+            StartCoroutine(AudioFade());
+        }
+
     }
 
-    public static IEnumerator StartFade(AudioSource source, float time, float goal)
+    public static IEnumerator FadeIn(AudioSource source, float time, float goal)
     {
         Debug.Log("Inicia rutina");
         float currentTime = 0;
@@ -53,9 +66,46 @@ public class VolumeControler : MonoBehaviour
             Debug.Log("Baja el volumen");
             currentTime += Time.deltaTime;
             //El 0 es el volumen al que queremos llegar podríamos hacer una variable
-            source.volume = Mathf.Lerp(start, goal, currentTime / time);
+            //source.volume = Mathf.Lerp(start, goal, currentTime / time);
+            source.volume = (float) Math.Sqrt((double)currentTime / (double)time );
             yield return null;
         }
+
         yield return null;
     }
+    public static IEnumerator FadeOut(AudioSource source, float time, float goal)
+    {
+        Debug.Log("Inicia rutina");
+        float currentTime = 0;
+        float start = source.volume;
+
+        while (currentTime < time)
+        {
+            Debug.Log("Baja el volumen");
+            currentTime += Time.deltaTime;
+            //El 0 es el volumen al que queremos llegar podríamos hacer una variable
+            //source.volume = Mathf.Lerp(start, goal, currentTime / time);
+            source.volume = (float)Math.Sqrt(((double) time - (double)currentTime) / (double)time);
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    public IEnumerator AudioFade()
+    {
+        canFade = false;
+        StartCoroutine(FadeIn(act, time, 0));
+        changeSong();
+        yield return StartCoroutine(FadeOut(act, time, 1));
+        canFade = true;
+
+    }
+    
+
+    void changeSong()
+    { 
+        act = source[UnityEngine.Random.Range(0, source.Length)];
+    }
+
 }
